11e9f0dc41b8ba433e7e3d18033ff354443d5243
ymaidana <ymaidana@skytouchtechnology.com>
2022-08-24 10:30:44 -0500

    first question

---
+++ homeworks/week_1.py
@@ -0,0 +1,13 @@
+from components.resistance import Resistance
+
+n = 20
+lg = 0.1e-3
+#mean_length_per_turn
+MLT = 14e-2
+
+wire_length = MLT*n
+area = 13.07e-5
+
+R = Resistance(wire_length, area)
+
+print("Winding resistance {:e} ohms".format(R.value))
--- main.py
+++ main.py
@@ -11,6 +11,5 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    import homeworks.week_1
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
