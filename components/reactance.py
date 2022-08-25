2a6041d08a6a0425c1a2c101930cea3faedb85d0
ymaidana <ymaidana@skytouchtechnology.com>
2022-08-25 10:22:14 -0500

    Adding aritmetic functions and parallel operator

--- components/reactance.py
+++ components/reactance.py
@@ -8,3 +8,21 @@ class Reactance:
     def __init__(self, large, cross_section_area, relative_permittivity=1):
         permittivity = relative_permittivity * vacuum_permittivity
         self.reactance = large / (permittivity * cross_section_area)
+
+    def __add__(self, other):
+        new_reactance = Reactance(1, 1)
+        new_reactance.reactance = self.reactance + other.reactance
+        return new_reactance
+
+    def parallel(self, other):
+        new_reactance = Reactance(1, 1)
+        new_reactance.reactance = 1/(1/self.reactance + 1/other.reactance)
+        return new_reactance
+
+    def __mul__(self, other):
+        new_reactance = Reactance(1, 1)
+        if other is Reactance:
+            new_reactance.reactance = self.reactance * other.reactance
+        else:
+            new_reactance.reactance = self.reactance * other
+        return new_reactance
